7af06e7b54c683e9406d53315e6b6f42f76eec13
Gustavo Silveira Frehse <gusfrehse@gmail.com>
2019-01-24 11:44:35 -0200

    This is how the mafia works boi. Now it prints the tree in a more intuitive way

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 This is a prefix tree (https://en.wikipedia.org/wiki/Trie), I think. I was 
-planning on doing a Huffman tree, but then I realized that is a prefix tree.
+planning on doing a Huffman tree, but then realized that this is a prefix tree.
 I don't know if the terminology is correct, and I am pretty sure that there
 are bugs (that I am yet to discover, so if you find one contact me).
 I want to make an implemantation of the Huffman coding, so I can compress 
@@ -12,11 +12,19 @@ class Node:
     dependencies = []
     frequency = 0
     name = "unnamed"
+    totalDepen = 0
 
-    def __init__(self, depen, frequ, nam):
+    def __init__(self, depen, frequ, nam, totalDepe):
         self.frequency = frequ
         self.dependencies = depen
+        self.totalDepen = totalDepe
         self.name = nam
+
+    def printTreeFormat(self, tabs, binaryCode):
+    # Print in a fancy tree format
+        print("    " * tabs + binaryCode +  " " + self.name + " " + str(self.frequency))
+        for i in range(len(self.dependencies)):
+            self.dependencies[i].printTreeFormat(tabs + 1, binaryCode + str(i))
 
 def getNodeFrequency(node):
     # Method so I can sort based on the frequency
@@ -25,7 +33,7 @@ def getNodeFrequency(node):
 def concatenateNodes(nodesList, nodes):
     print("concatenating {} ({}) and {} ({})".format(nodes[0].name, nodes[0].frequency, nodes[1].name, nodes[0].frequency))
     # Create a node that is the combinations of those with least frequency
-    n = Node(nodes, nodes[0].frequency + nodes[1].frequency, nodes[0].name + nodes[1].name)
+    n = Node(nodes, nodes[0].frequency + nodes[1].frequency, nodes[0].name + nodes[1].name, nodes[0].totalDepen + nodes[0].totalDepen)
     # Remove the nodes that were concatenated
     for node in nodes:
         nodesList.remove(node)
@@ -51,7 +59,7 @@ nodes = []
 with open(sys.argv[1], "r") as f:
     for line in f:
         (frequency, name) = line.split()
-        nodes.append(Node([], float(frequency), name))
+        nodes.append(Node([], float(frequency), name, 0))
 
 while len(nodes) > 1:
     # Print
@@ -62,5 +70,6 @@ while len(nodes) > 1:
     # Concatenate the two least frequent
     concatenateNodes(nodes, [nodes[0],nodes[1]])
 
-printNodeNames(nodes)
-print(nodes[0].frequency)
+for i in range(len(nodes)):
+    nodes[i].printTreeFormat(0, "")
+
